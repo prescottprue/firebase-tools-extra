8a8f01b3f6f878fa301d88dcf93c772a26e92234
Scott Prue <prescottprue@users.noreply.github.com>
2019-06-11 00:02:03 -0500

    v0.1.0-alpha (#5)
    * feat(core): drop node 6 support
    * feat(core): remove npx and reference firebase from bin instead
    * feat(core): update firebase-admin to v8
    * feat(lint): add prettier config to lint settings

--- src/constants.js
+++ src/constants.js
@@ -5,7 +5,7 @@ export const DEFAULT_TEST_ENV_FILE_PATH = 'cypress.env.json';
 export const DEFAULT_CONFIG_FILE_PATH = 'config.json';
 // Path for firebase-tools command (npx is used so that local version is used by default
 // falling back to the globally installed instance)
-export const FIREBASE_TOOLS_BASE_COMMAND = 'npx firebase';
+export const FIREBASE_TOOLS_BASE_COMMAND = '$(npm bin)/firebase';
 // Path to Firebase Extra Command Line tool (wrapper for firebase-tools)
 export const FIREBASE_EXTRA_PATH = '$(npm bin)/firebase-extra';
 export const FIREBASE_TOOLS_YES_ARGUMENT = '-y';
