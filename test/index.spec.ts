f99a90a226b7ea849b2bb2a5e2dc5faa76712898
Scott Prue <scott@residebrokerage.com>
2019-12-06 08:06:52 -0500

    chore(tests): remove tests since they are not fully set up

--- test/index.spec.ts
+++ test/index.spec.ts
@@ -1,73 +1,15 @@
 import firestoreAction from '../src/commands/firestore'
 import * as firebase from '@firebase/testing'
 import { expect } from 'chai'
-import { spawn } from 'child_process'
-import stream from 'stream';
 
 let app = firebase.initializeTestApp({
   projectId: "test",
   auth: { uid: "alice", email: "alice@example.com" }
 });
 
-export interface RunCommandOptions {
-  pipeOutput?: boolean;
-}
-
-
-/**
- * @description Run a bash command using spawn pipeing the results to the main
- * process
- * @param {String} command - Command to be executed
- * @private
- */
-export function runCommand(command: string, args?: string[], options?: RunCommandOptions) : Promise<any> {
-  return new Promise((resolve, reject): void => {
-    const child = spawn(command, args);
-    let output: any;
-    let error: any;
-    const customStream = new stream.Writable();
-    const customErrorStream = new stream.Writable();
-    /* eslint-disable no-underscore-dangle */
-    customStream._write = (data, ...argv): void => {
-      output += data;
-      if (options && options.pipeOutput) {
-        process.stdout._write(data, ...argv);
-      }
-    };
-    customErrorStream._write = (data, ...argv): void => {
-      error += data;
-      if (options && options.pipeOutput) {
-        process.stderr._write(data, ...argv);
-      }
-    };
-    /* eslint-enable no-underscore-dangle */
-    // Pipe errors and console output to main process
-    child.stdout.pipe(customStream);
-    child.stderr.pipe(customErrorStream);
-    // When child exits resolve or reject based on code
-    child.on('exit', (code: number): void => {
-      if (code !== 0) {
-        // Resolve for npm warnings
-        if (output && output.indexOf('npm WARN') !== -1) {
-          return resolve(output);
-        }
-        reject(error || output);
-      } else {
-        // Remove leading undefined from response
-        if (output && output.indexOf('undefined') === 0) {
-          resolve(output.replace('undefined', ''));
-        } else {
-          console.log('output: ', output); // eslint-disable-line no-console
-          resolve(output);
-        }
-      }
-    });
-  });
-}
-
 describe('firestoreAction', () => {
   before(() => {
-    process.env.FIRESTORE_EMULATOR_HOST="localhost:8080"
+    // process.env.FIRESTORE_EMULATOR_HOST="localhost:8080"
   })
 
   after(() => {
